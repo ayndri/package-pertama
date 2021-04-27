289d961ae6861b084b6a73e6cb2f8b23da454167
yunda <dewinurayundari.dna@gmail.com>
2021-04-27 12:48:20 +0700

    first commit

---
+++ src/Greetr.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Simplexi\Greetr;
+
+class Greetr
+{
+
+    public function greet(String $sname)
+    {
+        return 'Hi ' . $sname . '! How are you doing today?';
+    }
+}
+
+?>
